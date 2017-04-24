01652154325ac323baf236a59f92d40a28fa4263
Payer Ahmed <mdpayer125@gmail.com>
2017-04-24 23:00:19 +0600

    new

---
+++ app.js
@@ -0,0 +1,24 @@
+reddit.top('wallpaper').limit(12).fetch(function(res){
+
+var linkArr = res.data.children.mep(r => r.data.url);
+var nailArr = res.data.children.mep(n => n.data.thumbnail);
+
+var ul = document.querySelector('#ul');
+ linkArr.forEach(function(n,i){
+     var a = document.createElement('a');
+     var li = document.createElement('div');
+     var img= document.createElement('img');
+     a.href = n ;
+     img.src = nailArr[i];
+
+     a.appendChild(img);
+
+     li.appendChild(a);
+
+     ul.appendChild(li);
+
+
+ });
+
+
+});
